a490cfa6b4719047b1599edbbd74d162a1e24698
Rainer Stropek <rainer@software-architects.at>
2025-11-20 16:04:32 +0100

    Add Vite to Aspire

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -2,4 +2,9 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var webapi = builder.AddProject<Projects.WebApi>("webapi");
 
+// This is new in .NET 10: Vite & Python support
+var frontend = builder.AddViteApp("frontend", "../Frontend")
+    .WithReference(webapi)
+    .WithExternalHttpEndpoints();
+
 builder.Build().Run();
