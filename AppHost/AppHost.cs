276561801f7c3c913c83bd922298caa6487edef3
Rainer Stropek <rainer@software-architects.at>
2025-11-20 16:40:50 +0100

    Add database

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -1,6 +1,15 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var webapi = builder.AddProject<Projects.WebApi>("webapi");
+var sqlite = builder.AddSqlite(
+    "database",
+    builder.Configuration["Database:path"],
+    builder.Configuration["Database:fileName"])
+    .WithSqliteWeb(); // optionally add web admin UI (requires Docker/podman)
+
+var webapi = builder.AddProject<Projects.WebApi>("webapi")
+    .WithEnvironment("ConnectionStrings__Database", $"Data Source={Path.Combine(
+        builder.Configuration["Database:path"]!, 
+        builder.Configuration["Database:fileName"]!)}");
 
 // This is new in .NET 10: Vite & Python support
 var frontend = builder.AddViteApp("frontend", "../Frontend")
